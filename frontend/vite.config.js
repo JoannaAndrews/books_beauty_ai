7ebb3f506953d9b671a4f7d6936ba5220b8cee0b
Joanna <joanna.andrews284@gmail.com>
2026-03-26 00:55:33 -0400

    Frontend setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000"
+        target: "https://books-beauty-ai.onrender.com"//"http://localhost:5000"
       }
     }
   }
